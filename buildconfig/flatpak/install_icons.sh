07c02374d2ffe1deef22690d3e30e447f4513732
Jaskrendix <tur3a.giorgio.menzi@gmail.com>
2025-03-24 16:52:02 +0100

    bump reqs

--- buildconfig/flatpak/install_icons.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Define the sizes you want to install
-sizes=(16 32 48 64 128)
-
-# Loop through each size and install the corresponding icon
-for size in "${sizes[@]}"; do
-    install -Dm755 "mods/tuxemon/gfx/icon_${size}.png" "/app/share/icons/hicolor/${size}x${size}/apps/icon.png"
-done
